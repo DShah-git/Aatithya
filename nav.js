86797813b256571ce2d0311c6b76d9dab0806739
Devansh Shah <88405970+DShah-git@users.noreply.github.com>
2024-09-17 20:01:22 +0000

    navbar update

--- nav.js
+++ nav.js
@@ -1,25 +1,25 @@
 let nav = document.getElementById("nav-container")
 let container = document.getElementById("container")
-
+let links = document.getElementsByClassName("a")
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    if(scroll>=container.clientHeight - 180 - (24*2)){
+    if(scroll>=container.clientHeight - 70 - (24*2)){
         nav.style.position = "fixed"
         nav.style.backgroundColor = "#F5F5DC"
         nav.style.border = "2px solid #A20606"
         nav.style.top = "1.5rem !important"
-        nav.style.right = "1.5rem !important"
-
+        nav.style.right = "2rem"
+        nav.style.fontSize = "1.25rem"
+        nav.style.boxShadow = "rgba(100, 100, 111, 1) 0px 7px 29px 0px;"
     }
     else{
         nav.style.position = "sticky"
         nav.style.backgroundColor = "transparent"
         nav.style.top = "1.5rem"
         nav.style.border = "0px"
-        nav.style.transition = "0"
-        nav.style.right = ""
-        nav.style.animation = ""
+        nav.style.fontSize = "1.5rem"
+        nav.style.boxShadow = ""
     }
 
     
