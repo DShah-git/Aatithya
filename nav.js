7aeddc90c6078143a38afaa134d52ef98006092b
Devansh Shah <dshah135@my.centennialcollege.ca>
2024-09-18 15:02:25 -0400

    banquet and more

--- nav.js
+++ nav.js
@@ -2,13 +2,19 @@ let nav = document.getElementById("nav-container")
 let container = document.getElementById("container")
 
 let images = document.getElementById("images")
+let imageLayout2 = document.getElementById("imageLayout2")
 let infoContainer = document.getElementById("infoContainer")
+let infoContainer2 = document.getElementById("infoContainer2")
+let headingContainer = document.getElementById("headingContainer")
+
+let about = document.getElementById("about")
+
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     if(scroll>=container.clientHeight - 70 - (24*2)){
         nav.style.position = "fixed"
-        nav.style.backgroundColor = "#F5F5DC"
+        nav.style.backgroundColor = "#e8e8cf"
         nav.style.border = "2px solid #A20606"
         nav.style.top = "1.5rem !important"
         nav.style.right = "2rem"
@@ -24,7 +30,8 @@ window.addEventListener("scroll", (event) => {
         nav.style.boxShadow = ""
     } 
 
-    console.log(scroll)
+
+    
 
     if(scroll>=600 && !images.classList.value.split(' ').includes('animate-down')){
         images.classList.add('animate-down')
@@ -34,6 +41,17 @@ window.addEventListener("scroll", (event) => {
         infoContainer.classList.add('animate-up')
     }
 
+    if(scroll>=1400 && !headingContainer.classList.value.split(' ').includes('animate-down')){
+        headingContainer.classList.add('animate-down')
+    }
+
+    if(scroll>=1450 && !imageLayout2.classList.value.split(' ').includes('animate-down')){
+        imageLayout2.classList.add('animate-down')
+    }
+
+    if(scroll>=1500 && !infoContainer2.classList.value.split(' ').includes('animate-up')){
+        infoContainer2.classList.add('animate-up')
+    }
 });
 
 
